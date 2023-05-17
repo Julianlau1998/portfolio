9af164ac49a82d17f76aee2449da484fdab041f9
julianlau1998 <Julian.lau@aol.com>
2023-05-17 17:50:45 +0200

    social icons

--- src/components/base/Social.vue
+++ src/components/base/Social.vue
@@ -2,16 +2,16 @@
   <div class="is-socials">
     <div>
       <a class="mx-4" href="https://github.com/Julianlau1998" target='_blank'>
-        <img src="/img/socials/github.svg" class="is-socials-icon" />
+        <img src="/img/socials/github.svg" alt="Github Icon" class="is-socials-icon" />
       </a>
       <a class="mx-4" href="https://de.linkedin.com/in/julian-lau-9944a120a" target='_blank'>
-        <img src="/img/socials/linkedin.svg" class="is-socials-icon" />
+        <img src="/img/socials/linkedin.svg" alt="LinkedIn Icon" class="is-socials-icon" />
       </a>
       <a class="mx-4" href="https://play.google.com/store/apps/dev?id=7241866750836063639&gl=DE" target='_blank'>
-        <img src="/img/socials/google.svg" class="is-socials-icon" />
+        <img src="/img/socials/google.svg" alt="Play Store Icon" class="is-socials-icon" />
       </a>
       <a class="mx-4" href="https://apps.apple.com/ch/developer/julian-lau/id1603050262?see-all=i-phonei-pad-apps" target='_blank'>
-        <img src="/img/socials/appStore.svg" class="is-socials-icon" />
+        <img src="/img/socials/appStore.svg" alt="App Store Icon" class="is-socials-icon" />
       </a>
     </div>
   </div>
